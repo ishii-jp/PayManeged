a9bad8babd58ce93e179fda1590b0955e31328c3
ishii-jp <sadaharu5goo@icloud.com>
2022-01-01 20:43:08 +0900

    コンストラクタ プロパティに書き換えました

--- app/Events/InputPaymentCompleted.php
+++ app/Events/InputPaymentCompleted.php
@@ -14,27 +14,19 @@ class InputPaymentCompleted
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public string $email;
-    public string $name;
-    public string $year;
-    public string $month;
-    public array $payments;
-    public string $paymentSum;
-
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct($email, $name, $year, $month, $payments, $paymentSum)
-    {
-        $this->email = $email;
-        $this->name = $name;
-        $this->year = $year;
-        $this->month = $month;
-        $this->payments = $payments;
-        $this->paymentSum = $paymentSum;
-    }
+    public function __construct(
+        public string $email,
+        public string $name,
+        public string $year,
+        public string $month,
+        public array $payments,
+        public string $paymentSum
+    ) {}
 
     /**
      * Get the channels the event should broadcast on.
--- app/Mail/Payment/Notification.php
+++ app/Mail/Payment/Notification.php
@@ -11,25 +11,18 @@ class Notification extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public string $name;
-    public string $year;
-    public string $month;
-    public array $payments;
-    public string $paymentSum;
-
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($name, $year, $month, $payments, $paymentSum)
-    {
-        $this->name = $name;
-        $this->year = $year;
-        $this->month = $month;
-        $this->payments = $payments;
-        $this->paymentSum = $paymentSum;
-    }
+    public function __construct(
+        public string $name,
+        public string $year,
+        public string $month,
+        public array $payments,
+        public string $paymentSum
+    ) {}
 
     /**
      * Build the message.
