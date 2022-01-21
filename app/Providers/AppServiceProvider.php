872305242da54f562ec9538948e768354c511c08
Kunitaka Ishii <sadaharu5goo@icloud.com>
2022-01-21 12:01:51 +0900

    クエリーログをlaravel.logに出すようにしました

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // storage/logs/laravel.logでクエリーログを出す設定
+        if (config('app.env') !== 'production') {
+            \DB::listen(function ($query) {
+                \Log::info("[Query Time:{$query->time}s] $query->sql");
+            });
+        }
     }
 }
